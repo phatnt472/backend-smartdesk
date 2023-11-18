738f865b8d0da5d3c30ec881015af18c80969e1b
phatnt472 <20521737@gm.uit.edu.vn>
2023-11-18 19:48:24 +0700

    modify folder

--- recognition/test.py
+++ recognition/test.py
@@ -1,9 +1,14 @@
-import cv2
-import numpy as np
+from keras_facenet import FaceNet
+from numpy import load
+embedder = FaceNet()
+data = load('./6_faces_dataset.npz')
+print(data)
+# Gets a detection dict for each face
+# in an image. Each one has the bounding box and
+# face landmarks (from mtcnn.MTCNN) along with
+# the embedding from FaceNet.
 
-import glob
 
-from keras.models import load_model
-
-FRmodel = load_model('models/face-rec_Google.h5')
-print("Total Params:", FRmodel.count_params())
+# If you have pre-cropped images, you can skip the
+# detection step.
+embeddings = embedder.embeddings(images)
